63a0d832f6d10242d62ddf8656ab489c39391bd4
Juscier <jjmamore@gmail.com>
2025-01-10 19:20:59 -0300

    item ma205

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
+const Ma205 = React.lazy(() => import('./views/ma205/Ma205'))
 
 
 const Colors = React.lazy(() => import('./views/theme/colors/Colors'))
@@ -56,6 +57,7 @@ const Widgets = React.lazy(() => import('./views/widgets/Widgets'))
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
+  { path: '/ma205', name: 'Ma205', element: Ma205 },
   { path: '/theme', name: 'Theme', element: Colors, exact: true },
   { path: '/theme/colors', name: 'Colors', element: Colors },
   { path: '/theme/typography', name: 'Typography', element: Typography },
