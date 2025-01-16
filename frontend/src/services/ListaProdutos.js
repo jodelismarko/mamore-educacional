4f434ac060f751bf9af723aeeb6690fc49ade72d
jodelismarko <jodelismarko@gmail.com>
2025-01-16 07:42:35 -0400

    Lista de produtos

--- frontend/src/services/ListaProdutos.js
+++ frontend/src/services/ListaProdutos.js
@@ -3,6 +3,11 @@ export const produtos = [
         nome: 'Primeiro produto', 
         codigo: 'ma206', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
@@ -12,6 +17,11 @@ export const produtos = [
         nome: 'Segundo produto', 
         codigo: 'ma205', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
@@ -21,6 +31,11 @@ export const produtos = [
         nome: 'Terceiro produto', 
         codigo: 'ma207', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg',
@@ -30,6 +45,11 @@ export const produtos = [
         nome: 'Quarto produto', 
         codigo: 'ma208', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria4.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria4.jpg',
@@ -39,6 +59,11 @@ export const produtos = [
         nome: 'Quinto produto', 
         codigo: 'ma209', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria5.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria5.jpg',
@@ -48,6 +73,11 @@ export const produtos = [
         nome: 'Sexto produto', 
         codigo: 'ma210', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria6.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria6.jpg',
@@ -57,6 +87,11 @@ export const produtos = [
         nome: 'Setimo produto', 
         codigo: 'ma211', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria7.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria7.jpg',
@@ -66,6 +101,11 @@ export const produtos = [
         nome: 'Oitavo produto', 
         codigo: 'ma212', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria8.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria8.jpg',
@@ -75,6 +115,11 @@ export const produtos = [
         nome: 'Nono produto', 
         codigo: 'ma213', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria9.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria9.jpg',
@@ -84,6 +129,11 @@ export const produtos = [
         nome: 'Decimo produto', 
         codigo: 'ma214', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria10.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria10.jpg',
@@ -93,6 +143,11 @@ export const produtos = [
         nome: 'Primeiro produto', 
         codigo: 'ma2016', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria11.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria11.jpg',
@@ -102,6 +157,11 @@ export const produtos = [
         nome: 'Segundo produto', 
         codigo: 'ma2015', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria12.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria12.jpg',
@@ -111,6 +171,11 @@ export const produtos = [
         nome: 'Terceiro produto', 
         codigo: 'ma2017', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria13.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria13.jpg',
@@ -120,6 +185,11 @@ export const produtos = [
         nome: 'Quarto produto', 
         codigo: 'ma2018', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria14.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria14.jpg',
@@ -129,6 +199,11 @@ export const produtos = [
         nome: 'Quinto produto', 
         codigo: 'ma2019', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria15.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria15.jpg',
@@ -138,6 +213,11 @@ export const produtos = [
         nome: 'Sexto produto', 
         codigo: 'ma210', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria6.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria6.jpg',
@@ -147,6 +227,11 @@ export const produtos = [
         nome: 'Setimo produto', 
         codigo: 'ma211', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria7.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria7.jpg',
@@ -156,6 +241,11 @@ export const produtos = [
         nome: 'Oitavo produto', 
         codigo: 'ma212', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria8.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria8.jpg',
@@ -165,6 +255,11 @@ export const produtos = [
         nome: 'Nono produto', 
         codigo: 'ma213', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria9.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria9.jpg',
@@ -174,6 +269,11 @@ export const produtos = [
         nome: 'Decimo produto', 
         codigo: 'ma214', 
         descricao: 'Descrição do produto',
+        detalhes:[
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria1.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria2.jpg',
+            'https://primefaces.org/cdn/primereact/images/galleria/galleria3.jpg'
+        ],
         imagens: {
             original: 'https://primefaces.org/cdn/primereact/images/galleria/galleria10.jpg',
             miniatura: 'https://primefaces.org/cdn/primereact/images/galleria/galleria10.jpg',
